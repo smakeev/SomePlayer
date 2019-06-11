250d859b56a11eacb5c49d759a438300523af78d
Sergey Makeev <makeev.87@gmail.com>
2019-06-12 00:56:03 +0300

    Fix issues with ui representation of hours in timing ui representation

--- AudioStreamer/UI/TimeInterval+MMSS.swift
+++ AudioStreamer/UI/TimeInterval+MMSS.swift
@@ -9,15 +9,27 @@
 import Foundation
 
 extension TimeInterval {
-    
-    /// Converts a `TimeInterval` into a MM:SS formatted string.
-    ///
-    /// - Returns: A `String` representing the MM:SS formatted representation of the time interval.
-    public func toMMSS() -> String {
-        let ts = Int(self)
-        let s = ts % 60
-        let m = (ts / 60) % 60
-        return String(format: "%02d:%02d", m, s)
-    }
-    
+	
+	/// Converts a `TimeInterval` into a MM:SS formatted string.
+	///
+	/// - Returns: A `String` representing the MM:SS formatted representation of the time interval.
+	public func toMMSS() -> String {
+		if self >= 60 * 60 {
+			return toHHMMSS()
+		}
+		let ts = Int(self)
+		let s  = ts % 60
+		let m  = (ts / 60) % 60
+		return String(format: "%02d:%02d", m, s)
+	}
+	
+	public func toHHMMSS() -> String {
+		let ts = Int(self)
+		
+		let s  = ts % 60
+		let m  = (ts / 60) % 60
+		let h  = (ts / 60) / 60
+		return String(format: "%02d:%02d:%02d", h, m, s)
+	}
+	
 }
