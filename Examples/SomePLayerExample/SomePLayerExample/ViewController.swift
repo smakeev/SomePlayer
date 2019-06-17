8796bc39ab9c4f1078973535f3c8588293a9cddf
Sergey Makeev <makeev@freeconferencecall.com>
2019-06-17 16:29:03 +0300

    Start working on Player framework and it's example.

---
+++ Examples/SomePLayerExample/SomePLayerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomePLayerExample
+//
+//  Created by Sergey Makeev on 17/06/2019.
+//  Copyright © 2019 Sergey Makeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ SomePLayer/SomePLayer.h
@@ -0,0 +1,19 @@
+//
+//  SomePLayer.h
+//  SomePLayer
+//
+//  Created by Sergey Makeev on 17/06/2019.
+//  Copyright © 2019 Sergey Makeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SomePLayer.
+FOUNDATION_EXPORT double SomePLayerVersionNumber;
+
+//! Project version string for SomePLayer.
+FOUNDATION_EXPORT const unsigned char SomePLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomePLayer/PublicHeader.h>
+
+
