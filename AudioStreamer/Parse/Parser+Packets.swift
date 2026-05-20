7a993adfdfe73f321916357ab2121af9d42fa3c7
Sergey Makeev <makeev.87@gmail.com>
2026-05-20 17:17:31 +0500

    Build fix for new xcode

--- AudioStreamer/Parse/Parser+Packets.swift
+++ AudioStreamer/Parse/Parser+Packets.swift
@@ -9,7 +9,12 @@ import Foundation
 import AVFoundation
 import os.log
 
-func ParserPacketCallback(_ context: UnsafeMutableRawPointer, _ byteCount: UInt32, _ packetCount: UInt32, _ data: UnsafeRawPointer, _ packetDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>) {
+func ParserPacketCallback(_ context: UnsafeMutableRawPointer,
+						  _ byteCount: UInt32,
+						  _ packetCount: UInt32,
+						  _ data: UnsafeRawPointer,
+						  _ packetDescriptions: UnsafeMutablePointer<AudioStreamPacketDescription>?
+) {
     let parser = Unmanaged<Parser>.fromOpaque(context).takeUnretainedValue()
     let packetDescriptionsOrNil: UnsafeMutablePointer<AudioStreamPacketDescription>? = packetDescriptions
     let isCompressed = packetDescriptionsOrNil != nil
@@ -23,11 +28,12 @@ func ParserPacketCallback(_ context: UnsafeMutableRawPointer, _ byteCount: UInt3
     /// Iterate through the packets and store the data appropriately
     if isCompressed {
         for i in 0 ..< Int(packetCount) {
-            let packetDescription = packetDescriptions[i]
-            let packetStart = Int(packetDescription.mStartOffset)
-            let packetSize = Int(packetDescription.mDataByteSize)
-            let packetData = Data(bytes: data.advanced(by: packetStart), count: packetSize)
-            parser.packets.append((packetData, packetDescription))
+			if let packetDescription = packetDescriptions?[i] {
+				let packetStart = Int(packetDescription.mStartOffset)
+				let packetSize = Int(packetDescription.mDataByteSize)
+				let packetData = Data(bytes: data.advanced(by: packetStart), count: packetSize)
+				parser.packets.append((packetData, packetDescription))
+			}
         }
     } else {
         let format = dataFormat.streamDescription.pointee
